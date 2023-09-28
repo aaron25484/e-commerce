a4e2e9e583cfe58bd2e71576bbac3af596b911ad
Aaron de los Santos <aaronjournalist@hotmail.com>
2023-09-28 23:01:58 +0200

    add footer files

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import './footer.css'
+const Footer = () => {
+    return (
+      <footer className="p-4 bg-yellow-500 text-white text-center">
+        <p className="black">© Fuzz Records</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
