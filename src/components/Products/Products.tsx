3ac87d3bf00f95dfbfdacf2be15a20089e5003ed
Aaron de los Santos <aaronjournalist@hotmail.com>
2023-09-28 23:05:40 +0200

    add: product file

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,30 +1,34 @@
-import { Fragment, FC, useState } from "react";
+import { FC, useState } from "react";
 
-type ProductsProps = {
-    // image: string;
+export type ProductsProps = {
+  album: {
+    id: string;
+    name: string;
+    image: string;
     artist: string;
-    title: string;
-    price: number;
-    counter?: number
+    price: string;
+  };
+};
 
-}
+export const Products: FC<ProductsProps> = ({ album }) => {
+  const [counter, setCounter] = useState<number>(0);
 
-export const Products: FC<ProductsProps> = ({ artist, title, price}) => {
-    const [counter, setCounter] = useState<number>(0);
+  const handleIncrease = () => {
+    setCounter((prevState) => prevState + 1);
+  };
 
-    const handleIncrease = () => {
-        setCounter((prevState) => prevState + 1);
-    }
-
-    return(
-        <Fragment>
-            <img src="" alt="" />
-            <h5 className='text-red-700 text-3xl'>{artist}</h5>
-            <h4>{title}</h4>
-            <h5>{price}</h5>
-            <button onClick={handleIncrease}>+</button>
-            <p>{counter}</p>
-        </Fragment>
-    );
-
+  return (
+    <div className="product">
+      <img src={album.image} alt={album.name} className="product-image" />
+      <div className="product-info">
+        <h5 className="product-artist">{album.artist}</h5>
+        <h1 className="product-title">{album.name}</h1>
+        <h5 className="product-price">${album.price}</h5>
+        <div className="product-add-to-cart">
+          <button onClick={handleIncrease}>Add to Cart</button>
+          <p className="product-counter">{counter}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
